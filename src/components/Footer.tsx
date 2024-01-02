13a67e0e5ddd0956ddb95d2c261677b07be44056
noahdev0 <96663091+noahdev0@users.noreply.github.com>
2024-01-03 00:36:17 +0100

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,10 @@ import React from "react";
 import {
   GrFacebook,
   GrFacebookOption,
+  GrGithub,
   GrInstagram,
   GrLinkedin,
+  GrMail,
   GrTwitter,
   GrYoutube,
 } from "react-icons/gr";
@@ -13,18 +15,22 @@ import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import Image from "next/image";
 import { Separator } from "@radix-ui/react-separator";
+import { GiPhone } from "react-icons/gi";
 
 type Props = {};
 
 const Footer = (props: Props) => {
   return (
-    <footer className="bg-primary">
+    <footer className="bg-primary py-7">
       <div
         className="
         container
+        
+        w-full
         flex flex-col flex-wrap
         px-4
-        py-16
+        
+        
         mx-auto
         md:items-center
         lg:items-start
@@ -47,81 +53,67 @@ const Footer = (props: Props) => {
             <Button variant="destructive">Subscribe</Button>
           </div>
           <div className="flex mt-4 space-x-4 lg:mt-2 text-xl">
-            <Link href={""}>
+            <Link
+              href={"https://www.facebook.com/profile.php?id=61550225428516"}
+            >
               <GrFacebookOption className="text-gray-100" />
             </Link>
-            <Link href={""}>
+            <Link
+              href={"https://www.facebook.com/profile.php?id=61550225428516"}
+            >
               <GrTwitter className="text-gray-100" />
             </Link>
-            <Link href={""}>
+            <Link
+              href={"https://www.youtube.com/channel/UCVMN5tTizPkj2UN4Csj2MzQ"}
+            >
               <GrYoutube className="text-gray-100" />
             </Link>
-            <Link href={""}>
+            <Link href={"https://www.linkedin.com/company/csen-dz"}>
               <GrLinkedin className="text-gray-100" />
+            </Link>
+            <Link href={"https://github.com/CSEN-Dz"}>
+              <GrGithub className="text-gray-100" />
             </Link>
           </div>
         </div>
-        <div className="justify-around w-full mt-4 text-center lg:flex">
-          <div className="w-full px-4 lg:w-1/3 md:w-1/2">
-            <h2 className="mb-2 font-bold tracking-widest text-gray-100">
-              Quick Links
+        <div className="justify-around items-center w-full mt-4 text-center lg:flex">
+          <div className="w-full h-full px-4 lg:w-1/3 md:w-1/2">
+            <h2 className="mb-2 font-semibold tracking-widest text-gray-100">
+              Partners
             </h2>
-            <ul className="mb-8 space-y-2 text-sm list-none">
+            <ul className="space-y-2 text-sm list-none">
               <li>
-                <Link href={"/"} className="text-gray-300">
-                  Link 1
+                <Link
+                  href={"https://www.scte-dz.com/cms/"}
+                  className="text-gray-300"
+                >
+                  SCTE
                 </Link>
               </li>
               <li>
-                <Link href={"/"} className="text-gray-300">
-                  Link 2
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"} className="text-gray-300">
-                  Link 3
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"} className="text-gray-300">
-                  Link 4
+                <Link href={"www.anpt.dz"} className="text-gray-300">
+                  ANPT
                 </Link>
               </li>
             </ul>
           </div>
 
-          <Separator orientation="vertical" className=" w-[1px] bg-white/50" />
           <div className="w-full px-4 lg:w-1/3 md:w-1/2">
-            <h2 className="mb-2 font-bold tracking-widest text-gray-100">
-              Quick Links
+            <h2 className="mb-2 font-semibold tracking-widest text-gray-100">
+              <Link href={"mailto:contact@csen-dz.com"}>
+                <GrMail className="inline-block" /> contact@csen-dz.com
+              </Link>
             </h2>
-            <ul className="mb-8 space-y-2 text-sm list-none">
-              <li>
-                <Link href={"/"} className="text-gray-300">
-                  Link 1
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"} className="text-gray-300">
-                  Link 2
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"} className="text-gray-300">
-                  Link 3
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"} className="text-gray-300">
-                  Link 4
-                </Link>
-              </li>
-            </ul>
+            <h2 className="mb-2 font-semibold tracking-widest text-gray-100">
+              <Link href={"tel:+213 771 69 21 63"}>
+                <GiPhone className="inline-block" /> +213 771 69 21 63
+              </Link>
+            </h2>
           </div>
         </div>
       </div>
-      <div className="flex justify-center -mt-12">
-        <p className="text-center text-white pb-2">
+      <div className="flex justify-center items-center ">
+        <p className="text-center text-white  ">
           CSEN &copy; {new Date().getFullYear()} All rights reserved.
         </p>
       </div>
