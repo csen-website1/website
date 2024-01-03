a5f8e927f90e04eeeed865cfd0f5d15cba59586d
noahdev0 <96663091+noahdev0@users.noreply.github.com>
2024-01-03 07:46:51 +0100

    testing

--- src/app/api/submit/route.ts
+++ src/app/api/submit/route.ts
@@ -1,34 +1,34 @@
-import { NextResponse } from "next/server";
-import connectToDatabase from "@/lib/db";
-import UserModel from "@/models/User";
+// import { NextResponse } from "next/server";
+// import connectToDatabase from "@/lib/db";
+// // import UserModel from "@/models/User";
 
 export const POST = async (request: Request) => {
-  try {
-    await connectToDatabase();
-    const data = await request.json();
-    // const existingUser = await UserModel.findOne({
-    //   email: data.email,
-    // });
-    // if (existingUser !== null) {
-    //   data.message = [
-    //     {
-    //       text: data.message,
-    //       date: new Date(),
-    //     },
-    //   ];
-    //   await data.save();
-    //   return NextResponse.json({ message: "Message added" }, { status: 200 });
-    // }
-    const user = await UserModel.create({
-      ...data,
-      message: {
-        text: data.message,
-        date: new Date(),
-      },
-    });
+  // try {
+  //   await connectToDatabase();
+  //   const data = await request.json();
+  //   // const existingUser = await UserModel.findOne({
+  //   //   email: data.email,
+  //   // });
+  //   // if (existingUser !== null) {
+  //   //   data.message = [
+  //   //     {
+  //   //       text: data.message,
+  //   //       date: new Date(),
+  //   //     },
+  //   //   ];
+  //   //   await data.save();
+  //   //   return NextResponse.json({ message: "Message added" }, { status: 200 });
+  //   // }
+  //   const user = await UserModel.create({
+  //     ...data,
+  //     message: {
+  //       text: data.message,
+  //       date: new Date(),
+  //     },
+  //   });
 
-    return NextResponse.json({ message: "User added" }, { status: 200 });
-  } catch (error: any) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
+  //   return NextResponse.json({ message: "User added" }, { status: 200 });
+  // } catch (error: any) {
+  //   return NextResponse.json({ error: error.message }, { status: 500 });
+  // }
 };
