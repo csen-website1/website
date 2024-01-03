8c5be177cda2ea1e7d72ef2c8cc14f4b5c97132b
noahdev0 <96663091+noahdev0@users.noreply.github.com>
2024-01-04 00:28:33 +0100

    fomr updated

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -6,9 +6,10 @@ type Props = {};
 function page({}: Props) {
   return (
     <>
-      <h1 className="mx-auto">Register To RPA Plug-in</h1>
+      <h1 className="text-center text-transparent my-24 py-5 bg-gradient-to-r from-primary to-purple-300 font-semibold text-6xl bg-clip-text border ">
+        Register To RPA Plug-in
+      </h1>
       <Form />
-      
     </>
   );
 }
