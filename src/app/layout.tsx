2ecb6710ac5ad943e3b9f5d5b7d2f56dbd53942f
noahdev0 <96663091+noahdev0@users.noreply.github.com>
2024-01-03 22:26:45 +0100

    SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,25 @@ import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "CSEN",
-  description: "Company of Structural Engineering & Numerical",
+  title: "CSEN dz | Structural Engineering & Numerical Solutions", // Enhanced title with key services
+  description:
+    "CSEN dz provides innovative structural engineering and numerical analysis solutions for diverse industries. Explore our services, RPA Plug-in, and CSEN Cloud.", // More descriptive and keyword-rich
   icons: "/favicon.ico",
+  keywords:
+    "structural engineering, numerical analysis, RPA, CSEN Cloud, CSEN dz", // Refined keywords for relevance
+  openGraph: {
+    title: "CSEN dz | Structural Engineering & Numerical Solutions",
+    url: "https://csen-dz.com",
+    description:
+      "CSEN dz offers expert structural engineering and numerical analysis services to optimize your projects.",
+    images: ["/og-image.jpg"], // Add a visually appealing image
+  },
+  twitter: {
+    title: "CSEN dz: Empowering Innovation in Structural Engineering",
+    description:
+      "Discover CSEN dz's cutting-edge services for structural engineering and numerical analysis. #structuralengineering #numericalanalysis",
+    images: "/twitter-image.jpg", // Use a relevant Twitter image
+  },
 };
 export default function RootLayout({
   children,
