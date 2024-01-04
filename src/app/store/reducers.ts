10e8eb089407b837eb4e79621af1139f2e966e19
noahdev0 <96663091+noahdev0@users.noreply.github.com>
2024-01-04 01:56:23 +0100

    no ida

--- src/app/store/reducers.ts
+++ src/app/store/reducers.ts
@@ -1,33 +1,33 @@
 // reducers.ts
-import { Reducer } from "redux";
+import { Reducer } from 'redux';
 
 // Define the shape of the state
 interface AppState {
   error: string | null;
-  state: boolean;
+  state: string;
   text: string;
 }
 
 // Define the initial state
 const initialState: AppState = {
   error: null,
-  state: false,
-  text: "",
+  state: '',
+  text: '',
 };
 
 // Define actions with their types
 interface SetErrorAction {
-  type: "SET_ERROR";
+  type: 'SET_ERROR';
   payload: string; // The payload can be the error message
 }
 
 interface SetStateAction {
-  type: "SET_STATE";
-  payload: boolean; // The payload can be the new state
+  type: 'SET_STATE';
+  payload: string; // The payload can be the new state
 }
 
 interface SetTextAction {
-  type: "SET_TEXT";
+  type: 'SET_TEXT';
   payload: string; // The payload can be the new text
 }
 
@@ -35,22 +35,19 @@ interface SetTextAction {
 type AppAction = SetErrorAction | SetStateAction | SetTextAction;
 
 // Create the reducer function
-const appReducer: Reducer<AppState, AppAction> = (
-  state = initialState,
-  action
-) => {
+const appReducer: Reducer<AppState, AppAction> = (state = initialState, action) => {
   switch (action.type) {
-    case "SET_ERROR":
+    case 'SET_ERROR':
       return {
         ...state,
         error: action.payload,
       };
-    case "SET_STATE":
+    case 'SET_STATE':
       return {
         ...state,
         state: action.payload,
       };
-    case "SET_TEXT":
+    case 'SET_TEXT':
       return {
         ...state,
         text: action.payload,
