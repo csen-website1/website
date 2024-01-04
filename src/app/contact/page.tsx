8c91d20bf19c0e350db174695e6370e48a057007
noahdev0 <96663091+noahdev0@users.noreply.github.com>
2024-01-04 01:58:13 +0100

    adding redux

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -11,8 +11,8 @@ function page({}: Props) {
       <h1 className="text-center text-transparent my-24 py-5 bg-gradient-to-r from-primary to-purple-300 font-semibold text-6xl bg-clip-text border ">
         Register To RPA Plug-in
       </h1>
-      {/* <Form /> */}
-      {/* <Accordions /> */}
+      <Form />
+      <Accordions />
       {/* <AlertComponent /> */}
     </>
   );
