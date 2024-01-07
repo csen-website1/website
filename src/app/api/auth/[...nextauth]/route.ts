607cf7c04bd6233027e71d48aff152a72d14fa1a
noahdev0 <96663091+noahdev0@users.noreply.github.com>
2024-01-07 21:33:15 +0100

    sahnoun

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,9 @@
+import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import nextAuth from "next-auth";
+import { config } from "./auth";
+
+const handler = NextAuth(config);
+
+export const authOptions = NextAuth(config);
+export { handler as GET, handler as POST };
---
+++ src/app/getData/route.ts
@@ -0,0 +1,18 @@
+// import { getServerSession } from "next-auth";
+// import { NextResponse } from "next/server";
+
+// import { authOptions } from "../api/auth/[...nextauth]/route";
+// import connectToDatabase from "@/lib/db";
+// import { UserModel } from "@/models/User";
+
+// export async function GET() {
+//   const session = (await getServerSession(authOptions)) as {
+//     user?: { name: string };
+//   };
+//   if (session?.user?.name === process.env.CSEN_ADMIN) {
+//     await connectToDatabase();
+//     const users = await UserModel.find({});
+//     return NextResponse.json(users);
+//   }
+//   return NextResponse.redirect("https://csen-dz.com");
+// }
