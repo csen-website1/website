79dd1a74edbe5bab05098b9f10cfdaf7eeaa1ded
noahdev0 <96663091+noahdev0@users.noreply.github.com>
2024-01-07 17:44:27 +0100

    idk

--- src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,9 +0,0 @@
-// import NextAuth from "next-auth";
-// import GitHubProvider from "next-auth/providers/github";
-// import nextAuth from "next-auth";
-// import { config } from "./auth";
-
-// const handler = NextAuth(config);
-
-// export const authOptions = NextAuth(config);
-// export { handler as GET, handler as POST };
