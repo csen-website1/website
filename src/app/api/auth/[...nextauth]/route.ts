4c60c3193cbc693cb92230e6c0bd9de33a6d5d14
noahdev0 <96663091+noahdev0@users.noreply.github.com>
2024-01-07 17:20:47 +0100

    added the email reply  on footer

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import nextAuth from "next-auth";
+import { config } from "./auth";
 
 const handler = NextAuth({
   providers: [
@@ -9,5 +11,6 @@ const handler = NextAuth({
     }),
   ],
 });
+export const authOptions = nextAuth(config);
 
 export { handler as GET, handler as POST };
