452d70e7fbf98eaf0f08c34ce3232c0294e74122
noahdev0 <96663091+noahdev0@users.noreply.github.com>
2024-06-24 02:55:32 +0100

    ed

--- src/app/api/getData/route.ts
+++ src/app/api/getData/route.ts
@@ -3,19 +3,19 @@ import { UserModel } from "@/models/User";
 import { auth } from "@/auth";
 import { NextResponse } from "next/server";
 
-export const GET = auth(async function GET(req) {
-  if (req.auth) {
-    try {
-      await connectToDatabase();
-      const users = await UserModel.find({}).sort({ createdAt: -1 });
-      return NextResponse.json(users);
-    } catch (error) {
-      return NextResponse.json(
-        { message: "Error fetching data", error: error },
-        { status: 500 }
-      );
-    }
+export async function GET(req: Request) {
+  const session = await auth();
+  if (!session) {
+    return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
   }
-
-  return NextResponse.json({ message: "Not authenticated" }, { status: 401 });
-});
+  try {
+    await connectToDatabase();
+    const users = await UserModel.find({}).sort({ createdAt: -1 });
+    return NextResponse.json(users);
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Error fetching data", error: error },
+      { status: 500 }
+    );
+  }
+}
